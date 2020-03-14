4f305f076423192c1b84ab379835714fef449bbd
Ian Lucas <ian.lucas@gmail.com>
2020-03-14 22:57:49 +0100

    add dynamic message to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+/*
+interface data {
+  id:string,
+  content:string;
+}
+*/
+
 export class AppComponent {
-  title = 'client';
+  title = 'Lucasia Tutorial';
+  // tslint:disable-next-line:ban-types
+  data: Object = {
+    id: undefined
+  };
+  constructor(private http: HttpClient) {
+    http.get('resource').subscribe(data => this.data = data);
+  }
 }
--- src/main/java/com/example/ui/UiApplication.java
+++ src/main/java/com/example/ui/UiApplication.java
@@ -2,12 +2,50 @@ package com.example.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
+@Controller
 public class UiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(UiApplication.class, args);
+  }
+
+  @GetMapping("/resource")
+  @ResponseBody
+  public Map<String, Object> home() {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("id", UUID.randomUUID().toString());
+    model.put("content", "Hello World");
+    return model;
+  }
+
+  @GetMapping(value = "/{path:[^\\.]*}")
+  public String redirect() {
+    return "forward:/";
+  }
+
+  @Component
+  @ConfigurationProperties("demo")
+  class DemoProperties {
+    private String value;
+
+    public String getValue() {
+      return value;
+    }
+
+    public void setValue(String value) {
+      this.value = value;
+    }
+  }
 
 }
